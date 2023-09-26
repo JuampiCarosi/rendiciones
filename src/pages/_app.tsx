77ca9dcc108e494d12f98740d61d6179bb9d9f61
juampicarosi <juampicarosi@gmail.com>
2023-09-26 12:53:24 -0300

    add page analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };
